dcd253784334b9784d3832ab4991cee52da53a8a
ktunchar <maikittitee1615@gmail.com>
2024-02-05 20:34:08 +0700

    4-3

---
+++ cell04/ex03/float.py
@@ -0,0 +1,22 @@
+def is_int(n):
+	try:
+		int(n)
+		return (True)
+	except ValueError:
+		return (False)
+	
+def is_float(n):
+	try:
+		float(n)
+		return(True)
+	except ValueError:
+		return (False)
+
+
+n = str(input("Give me a number: "))
+if (is_int(n)):
+	print("This number is an integer.")
+elif (is_float(n)):
+	print("This number is a decimal.")
+else:
+	print("this program take only numberic idiot !")
